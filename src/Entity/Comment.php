55e3ebe832202c295a0d69190b1e52417c32ac5b
Lorddistrict <etienne.crespi@laposte.net>
2019-04-19 16:05:46 +0200

    Add some elemnts for the admin panel

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -10,98 +11,94 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Comment
 {
-  /**
-   * @ORM\Id()
-   * @ORM\GeneratedValue()
-   * @ORM\Column(type="integer")
-   */
-  private $id;
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="comment", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $article;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
 
-  /**
-   * @ORM\Column(type="text")
-   */
-  private $content;
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
 
 
-  /**
-   * @ORM\Column(type="datetime")
-   */
-  private $created;
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
 
 
-  /**
-   * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="comment", cascade={"persist", "remove"})
-   * @ORM\JoinColumn(nullable=false)
-   */
-  private $article;
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
 
-  /**
-   * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
-   * @ORM\JoinColumn(nullable=false)
-   */
-  private $author;
+    public function setArticle(Article $article): self
+    {
+        $this->article = $article;
 
-
-  public function __construct()
-  {
-    $this->author = new ArrayCollection();
-  }
+        return $this;
+    }
 
 
-  public function getId(): ?int
-  {
-    return $this->id;
-  }
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
 
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
-  public function getContent(): ?string
-  {
-    return $this->content;
-  }
+        return $this;
+    }
 
-  public function setContent(string $content): self
-  {
-    $this->content = $content;
-
-    return $this;
-  }
-
-
-  public function getCreated(): ?\DateTimeInterface
-  {
-    return $this->created;
-  }
-
-  public function setCreated(\DateTimeInterface $created): self
-  {
-    $this->created = $created;
-
-    return $this;
-  }
-
-
-  public function getArticle(): ?Article
-  {
-    return $this->article;
-  }
-
-  public function setArticle(Article $article): self
-  {
-    $this->article = $article;
-
-    return $this;
-  }
-
-  public function getAuthor(): ?User
-  {
-    return $this->author;
-  }
-
-  public function setAuthor(?User $author): self
-  {
-    $this->author = $author;
-
-    return $this;
-  }
+    public function __toString(): string
+    {
+        return $this->getContent();
+    }
 }
